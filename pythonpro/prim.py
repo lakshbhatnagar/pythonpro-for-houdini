b77a18b13d4d54e1a5a6ec7ed4613e8758825a76
Laksh Bhatnagar <lakshbhatnagar@users.noreply.github.com>
2018-05-23 16:22:42 +0530

    Update prim.py
    Adding polygon torus and assigning default values to def

--- pythonpro/prim.py
+++ pythonpro/prim.py
@@ -5,16 +5,16 @@ import hou
 import sys
 
 # polygon sphere
-def polySphere(path, name, radius, subd):
+def polySphere(path, name="polySphere", radius=1, rowsDivision=13, colsDivision=24):
     sphere = hou.node(path).createNode('sphere') # default houdini sphere
     sphere.setName(name) # sphere name
     sphere.parm("type").set("polymesh") # setting it's type to polyMesh
-    sphere.parm("rows").set(subd) # rows division
-    sphere.parm("cols").set(subd) # columns division
+    sphere.parm("rows").set(rowsDivision) # rows division
+    sphere.parm("cols").set(colsDivision) # columns division
     sphere.parm("scale").set(radius) # sphere radius
 
 # polygon cube
-def polyCube(path, name, size, axisDivision):
+def polyCube(path, name="polyCube", size=1, axisDivision=4):
     box = hou.node(path).createNode('box') # default houdini box
     box.setName(name) # box name
     box.parm("type").set("polymesh") # setting it's type to polyMesh
@@ -24,7 +24,7 @@ def polyCube(path, name, size, axisDivision):
     box.parm("divrate3").set(axisDivision) # axis division 3
 
 # polygon tube
-def polyTube(path, name, radius, height, rowsDivision, colsDivision):
+def polyTube(path, name="polyTube", radius=1, height=1, rowsDivision=2, colsDivision=12):
     tube = hou.node(path).createNode('tube') # default houdini tube
     tube.setName(name) # tube name
     tube.parm("type").set("poly") # setting it's type to polygon
@@ -32,3 +32,14 @@ def polyTube(path, name, radius, height, rowsDivision, colsDivision):
     tube.parm("height").set(height) # tube height
     tube.parm("rows").set(rowsDivision) # rows division
     tube.parm("cols").set(colsDivision) # columns division
+
+# polygon torus
+def polyTorus(path, name="polyTorus", radiusX=1, radiusY=0.5, scale=1, rowsDivision=12, colsDivision=24):
+    torus = hou.node(path).createNode('torus')
+    torus.setName(name) # torus name
+    torus.parm("type").set("poly") # setting it's type to polygon
+    torus.parm("radx").set(radiusX) # xRadius
+    torus.parm("rady").set(radiusY) # yRadius
+    torus.parm("scale").set(scale) # scale
+    torus.parm("rows").set(rowsDivision) # rows division
+    torus.parm("cols").set(colsDivision) # columns division
