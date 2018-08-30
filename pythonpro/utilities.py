2b2d95a3689e442ba932e1fddb6531301ff6b779
Laksh Bhatnagar <lakshbhatnagar@users.noreply.github.com>
2018-08-30 16:53:12 +0530

    Adding importSetup Command

--- pythonpro/utilities.py
+++ pythonpro/utilities.py
@@ -1,5 +1,6 @@
 # importing houdini modules
 import hou
+import os
 
 # merge selected objects
 def mergeSelectedNodes():
@@ -21,3 +22,27 @@ def mergeSelectedNodes():
         hou.Node.setNextInput(createMergeNode, selNodes)
 
     return createMergeNode
+
+# import latest setup files
+
+def importSetup():
+    ask = hou.ui.readInput("Enter Element Name") [1]
+    pathDir = hou.ui.readInput("Enter Path") [1] 
+    listDir = os.listdir(pathDir)
+    sortedDir = sorted(listDir)
+    length = len(sortedDir)
+
+    i = 0
+    while i < length:
+        if(sortedDir[i].find(ask) == 12):
+            MyList = []
+            MyList.append(sortedDir[i])
+        else:
+            pass
+        i += 1
+        pass
+
+    houdiniSceneFile = pathDir + "/" + MyList[0] + "/" + "script" + "/" + MyList[0] + ".hip"
+    hou.hipFile.merge(houdiniSceneFile)
+    return houdiniSceneFile
+
